c5d6c6a23b867d438d0af39cbe894d01b91cb199
elena <ecanizaresj99@gmail.com>
2024-01-08 12:58:29 +0100

    Segundo commit del proyecto

--- EstructuraBBDD.java
+++ EstructuraBBDD.java
@@ -4,22 +4,22 @@ import android.provider.BaseColumns;
 public final class EstructuraBBDD {
 
     public static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE IF NOT EXISTS "+ EstructuraCampeonatoTorneo.TABLE_NAME_OPERAS +
-                    "(" + EstructuraOperas._ID + " integer PRIMARY KEY, "
-                    + EstructuraOperas.COLUMN_NAME_TITULO + " text, "
-                    + EstructuraOperas.COLUMN_NAME_COMPOSITOR + " text, "
-                    + EstructuraOperas.COLUMN_FOTO_COMPOSITOR + " integer, "
-                    + EstructuraOperas.COLUMN_FOTO_CAPTURA + " blob, "
-                    + EstructuraOperas.COLUMN_NAME_YEAR + " integer);";
+            "CREATE TABLE IF NOT EXISTS "+ EstructuraCampeonatoTorneo.TABLE_NAME_CAMPEONATOTORNEO +
+                    "(" + EstructuraCampeonatoTorneo._ID + " integer PRIMARY KEY, "
+                    + EstructuraCampeonatoTorneo.COLUMN_NAME_TITULO + " text, "
+                    + EstructuraCampeonatoTorneo.COLUMN_NAME_COMPOSITOR + " text, "
+                    + EstructuraCampeonatoTorneo.COLUMN_FOTO_COMPOSITOR + " integer, "
+                    + EstructuraCampeonatoTorneo.COLUMN_FOTO_CAPTURA + " blob, "
+                    + EstructuraCampeonatoTorneo.COLUMN_NAME_YEAR + " integer);";
 
     public static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS  " + EstructuraOperas.TABLE_NAME_OPERAS;
+            "DROP TABLE IF EXISTS  " + EstructuraCampeonatoTorneo.TABLE_NAME_CAMPEONATOTORNEO;
 
     private EstructuraBBDD() {}
 
     /* Clase interna que define la estructura de la tabla de operas */
     public static class EstructuraCampeonatoTorneo implements BaseColumns {
-        public static final String TABLE_NAME_OPERAS = "operas";
+        public static final String TABLE_NAME_CAMPEONATOTORNEO = "operas";
         public static final String COLUMN_NAME_TITULO = "titulo";
         public static final String COLUMN_NAME_COMPOSITOR = "compositor";
         public static final String COLUMN_FOTO_COMPOSITOR = "foto";
